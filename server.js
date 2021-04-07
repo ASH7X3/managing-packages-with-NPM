662321be4fc7164cb5a79922323e600ff934b02a
Shahir Ashraf Kodabaccus <shahir.kodabaccus@gmail.com>
2021-04-07 18:04:32 +0400

    update server.js

--- server.js
+++ server.js
@@ -58,6 +58,6 @@ app.use(function (err, req, res, next) {
   }
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT, function () {
   console.log("Node.js listening ...");
 });
